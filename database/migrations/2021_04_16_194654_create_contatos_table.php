2559a1319f4fd6636be317add9f29827b6f7521e
André Soares Nascimento <andresoaresnascimento@gmail.com>
2021-04-16 20:32:50 -0300

    Editando o Arquivo de Migração Contato

--- database/migrations/2021_04_16_194654_create_contatos_table.php
+++ database/migrations/2021_04_16_194654_create_contatos_table.php
@@ -15,6 +15,13 @@ class CreateContatosTable extends Migration
     {
         Schema::create('contatos', function (Blueprint $table) {
             $table->id();
+            $table->string('nome');
+            $table->string('endereco');
+            $table->integer('codigo_pais');
+            $table->integer('codigo_cidade');
+            $table->integer('telefone');
+            $table->string('email');
+            $table->string('foto');
             $table->timestamps();
         });
     }
